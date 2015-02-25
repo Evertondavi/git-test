b8bf84c1431eade413daf19e8ccf734f63bbd866
41412257 <41412257@090028-P33S13.macklabs.br>
2015-02-25 10:02:46 -0300

    Troquei a cor do tia e do nome.

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  * and open the template in the editor.
  */
 
-  echo 12314;
-  echo '<h1 align="center">Everton</h1>';
+  echo '<h2 style="color:green;text-align:center;margin-top:300px;">41412257</h2>';
+  echo '<h1 style="color:red;text-align:center;" >Everton Davi</h1>';
 ?>
