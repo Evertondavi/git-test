f1c54d704bdefbbfa4aa0bb5b015cd58768f86be
Everton Davi <evertondavi1990@hotmail.com>
2015-02-26 11:52:57 -0300

    Tabela

---
+++ tabea.php
@@ -0,0 +1,51 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+           table tr td{ border: 1px solid #000;}
+            
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+               <td>ID</td> 
+               <td>NOME</td>
+            </tr>
+            <tr>
+               <td>id</td> 
+              <td>nome</td> 
+            </tr>
+            <tr>
+               <td>id</td> 
+               <td>nome</td>           
+            </tr>
+             <tr>
+               <td>id</td> 
+               <td>nome</td>           
+            </tr>
+             <tr>
+               <td>id</td> 
+               <td>nome</td>           
+            </tr>
+             <tr>
+               <td>id</td> 
+               <td>nome</td>           
+            </tr>
+            
+            
+        </table>
+        <?php
+        
+        
+        
+        
+        
+        ?>
+    </body>
+
+
