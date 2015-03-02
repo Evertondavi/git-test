d8923df9ce475be13a62c37108b6372c907d1fd9
Everton Davi <evertondavi1990@hotmail.com>
2015-03-02 12:42:35 -0300

    Tabela com cor pronta.

--- tabela.php
+++ tabela.php
@@ -1,51 +1,51 @@
 
-
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <style>
-           table tr td{ border: 1px solid #000;}
-            
-        </style>
-    </head>
-    <body>
-        <table>
-            <tr>
-               <td>ID</td> 
-               <td>NOME</td>
-            </tr>
-            <tr>
-               <td>id</td> 
-              <td>nome</td> 
-            </tr>
-            <tr>
-               <td>id</td> 
-               <td>nome</td>           
-            </tr>
-             <tr>
-               <td>id</td> 
-               <td>nome</td>           
-            </tr>
-             <tr>
-               <td>id</td> 
-               <td>nome</td>           
-            </tr>
-             <tr>
-               <td>id</td> 
-               <td>nome</td>           
-            </tr>
-            
-            
-        </table>
-        <?php
-        
-        
-        
-        
-        
-        ?>
-    </body>
 
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+
+<style>
+	table,tr,td{border: solid 1px  #000;}
+        .cor{background:gray;}
+</style>
+</head>
+<body>
+ <table>
+<?php
+   echo  
+        "  <tr>
+		<td>Nome</td>
+           	<td>ID</td>
+		<td>DESC</td>
+
+	</tr>";
+
+       for($i = 0; $i < 7; $i++){
+    
+        if(($i % 2 == 0)||($i == 0)){  
+         echo
+         "<tr>
+		<td>nome</td>
+           	<td>id</td>
+		<td>desc</td>
+	</tr>";
+        }else{
+           echo
+            "<tr class='cor'>
+		<td>nome</td>
+           	<td>id</td>
+		<td>desc</td>
+
+	</tr>"; 
+            
+            
+        }
+	}
+?>
+     </table>
+
+</body>
+</html>
 
